89731845318566267dc5f4d43fb3329ef84e9c33
João Victor <joao.victors1203@gmail.com>
2023-04-30 17:34:09 -0300

    scape line breaks on arguments

--- src/apiTracker/Monitor.java
+++ src/apiTracker/Monitor.java
@@ -2,14 +2,14 @@ package apiTracker;
 
 public class Monitor {
 
-	//private static boolean active = false;
+    //private static boolean active = false;
 
-	public static void apiCall (String methodSignature, Object[] params) {
+    public static void apiCall (String methodSignature, Object[] params) {
 		//if (active) return;
 		//active = true;
 		//try { 
-		apiCall_impl(methodSignature, params); 
-		//}
+        apiCall_impl(methodSignature, params); 
+        //}
 		//finally { active = false; }
 	}
 
@@ -17,21 +17,21 @@ public class Monitor {
 		try {
 			String args = "";
 			for (Object param : params) {
-				if (param == null) {
-						args += "null" + ", ";
-				} else {
-						args += "\"" + param.toString().replaceAll("\"", "\\\\\"") + "\", ";
-				}
+                if (param == null) {
+                    args += "null" + ", ";
+                } else {
+                    args += "\"" + param.toString().replaceAll("\"", "\\\\\"").replaceAll("\n", "\\\\\\\\n") + "\", ";
+                }
 			}
 
-			if (args.length() >= 2) {
-					args = args.substring(0, args.length() - 2);
-			}
+            if (args.length() >= 2) {
+                args = args.substring(0, args.length() - 2);
+            }
 
 			android.util.Log.i("apicall-monitor", "[API-TRACK]: " + methodSignature + " => " + args);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-		
+    
 }
